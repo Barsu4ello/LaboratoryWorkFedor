19c4e326f2c8a77b3583259efe0a365a97654c15
Vladislav Gorbunov <wint-mil@mail.ru>
2022-10-31 15:57:20 +0300

    One Test fix

--- src/test/java/com/cvetkov/fedor/laboratoryWork/controller/AudioTestController.java
+++ src/test/java/com/cvetkov/fedor/laboratoryWork/controller/AudioTestController.java
@@ -10,6 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.springframework.test.context.DynamicPropertyRegistry;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 import java.util.stream.Collectors;
